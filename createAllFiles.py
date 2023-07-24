e004eaed172dea3a34807f5bf9e1d56b429f8132
Jana <123444692+aresssera@users.noreply.github.com>
2023-07-24 08:26:21 +0200

    create createAllFiles.py

---
+++ createAllFiles.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import xml.etree.ElementTree as ET
+import csv
+
+fileName = 'input/allPossibleCombinations.csv'
+df = pd.read_csv(fileName)
+
+i = 1
+for template in df['Template'].unique():
+
+  print(i, ": ", template)
+
+  # all the attribute combinations which need to be with current template
+  tmpDF = df[df.Template == template]
+
+  for index, row in tmpDF.iterrows():
+
+    # load template
+    tree = ET.parse(template)
+    root = tree.getroot()
+
+    for att in valuesToReplace:
+
+      for element in root.iter():
+        #if element.text and element.text.startswith(att):
+        if element.text == att:
+          #if att == '-':
+          #  print("empty")
+          element.text = row[att]
+
+    # save modified XML file
+    tree.write('outputs/' + row['#VIN'] + '.xml', encoding='utf-8', xml_declaration=True)
+
+  i += 1
